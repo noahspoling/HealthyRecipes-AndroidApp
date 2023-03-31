feebd9bf3f224464e94d9c71a220ba09c2a39d0d
Noah Poling <xmosphere@gmail.com>
2023-03-31 13:57:37 -0400

    first commit

---
+++ app/src/main/java/net/androidbootcamp/healthyrecipes/MainActivity.java
@@ -0,0 +1,23 @@
+package net.androidbootcamp.healthyrecipes;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button =(Button) findViewById(R.id.btnRecipe);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Recipe.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/net/androidbootcamp/healthyrecipes/Recipe.java
@@ -0,0 +1,14 @@
+package net.androidbootcamp.healthyrecipes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Recipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+    }
+}
